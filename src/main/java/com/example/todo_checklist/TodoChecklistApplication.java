3525954d2e72c41bf12306aa1e98b3a468210588
Olly <olly.aaa@mail.ru>
2025-02-27 15:54:18 +0500

    создание чек-листа

---
+++ src/main/java/com/example/todo_checklist/TodoChecklistApplication.java
@@ -0,0 +1,13 @@
+package com.example.todo_checklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoChecklistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoChecklistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/todo_checklist/TodoChecklistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todo_checklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoChecklistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
